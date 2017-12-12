765262162519127abb5bdc89cdad8b9b803571a3
David Yu <yuqiwei_zju@163.com>
2017-12-12 23:26:16 +0800

    第一次提交： 编写了文件读取操作

---
+++ HumanModel/main.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	FILE *fp;	//定义一个名为fp的文件指针
+	if ((fp = fopen("  ", "r")) == NULL)
+	{
+		printf("Error:所要打开的文件不存在或格式错误\n");
+		system("pause");
+		return 0;
+	}
+}
